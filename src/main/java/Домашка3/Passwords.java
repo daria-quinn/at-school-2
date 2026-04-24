e2eaa05efecb4b9148d7c6920ccb5a5472776494
Дарья <DAkshentseva@alfabank.ru>
2026-04-24 12:49:39 +0300

    правка после код ревью

--- src/main/java/Домашка3/Passwords.java
+++ src/main/java/Домашка3/Passwords.java
@@ -2,13 +2,13 @@ package Домашка3;
 
 public class Passwords {
     public static void main (String[] args) {
-        String[] passwords = {"ty$gds#fdFSFG","12345678qwerty","4839jz"} ;
+        String[] passwords = {"ty$gds#fdFSFG","12345678QWERTY","4839jz","qwerty"} ;
          for (String pwd : passwords){
              boolean isValid = pwd.length() > 8 &&
                      !pwd.startsWith("1") &&
                      !pwd.endsWith("z") &&
                      !pwd.contains("1234") &&
-                     !pwd.contains("qwerty");
+                     !pwd.toLowerCase().contains("qwerty");
              System.out.println("пароль '" + pwd + "' прошел проверку: " + isValid);
          }
     }
