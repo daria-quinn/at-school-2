ae4d8261ed6bf1576675032e261b8f20db19e908
Дарья <DAkshentseva@alfabank.ru>
2026-05-29 10:53:59 +0300

    ДЗ8

--- src/main/java/Homework8/Winamp.java
+++ src/main/java/Homework8/Winamp.java
@@ -3,16 +3,21 @@ package Homework8;          // <-- тот же пакет, что и у Playlist
 
 public class Winamp {
     public static void main(String[] args) {
-        // Объект Playlist
+        // Создаем плейлист
         Playlist myPlaylist = new Playlist("Плейлист для vibe");
 
-        // Вызов всех методов
+        // Добавляем песни
         myPlaylist.addSong("Charli XCX -360");
         myPlaylist.addSong("Егор Кринж - Самая самая");
         myPlaylist.addSong("Лэйди Гага - Just Dance");
 
+        //Получаем первую песню в плейлисте
         myPlaylist.getSong(0);
+
+        //Обновляем песню
         myPlaylist.updateSong(2, "Егор Кринж, темный принц и ещё куча новых реперов - 67 новых песен");
+
+        //Удаляем вторую песню
         myPlaylist.removeSong(1);
 
         // Вывод финального состояния
